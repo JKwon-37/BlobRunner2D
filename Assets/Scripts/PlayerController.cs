17c996821f2b59a8fe596191f4ec2330c01d6cb6
veronica-hing <veronica.m.hing@gmail.com>
2022-06-28 18:50:14 -0700

    implemented respawn at checkpoint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -61,5 +61,9 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = respawnPoint;
         }
+        else if(collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
+        }
     }
 }
